247f60fbcf9e140cfb70b19ae77f139e44ad1fc8
Travis MacDonald (x2015khn) <x2015khn@stfx.ca>
2020-02-26 21:02:53 -0400

    User generation succesful

--- LFBSDataGenerator.py
+++ LFBSDataGenerator.py
@@ -1,10 +1,11 @@
 from random import seed
 from random import random
+import secrets
 
 
 class LFBSDataGenerator:
 
-    # Dataset lists
+    # Datasets
     _fNames = []
     _lNames = []
     _sports = []
@@ -16,7 +17,9 @@ class LFBSDataGenerator:
     _curId = 0
 
     def __init__(self):
+        self._ran = random()
         self._setup()
+
 
 
     """
@@ -32,13 +35,73 @@ class LFBSDataGenerator:
     Format: id,fName,lName,address
     """
     def _genUser(self):
-        return None
+        user = ""
+        user = self._genUserId() \
+               + ',' + self._genFirstName() \
+               + ',' + self._genLastName()
+        return user
+
+
+    def _genFirstName(self):
+       return secrets.choice(self._fNames)
+
+
+    def _genLastName(self):
+        return secrets.choice(self._lNames)
+
+
+    """
+    Generate a unique user ID
+    """
+    def _genUserId(self):
+        id = secrets.choice(self._idSuffix) + self._curId
+        self._curId += 1
+        return str(id)
+
 
 
     def __repr__(self):
         return "I am a Lost and Found / Buy and Sell random data generator"
 
+
     """
     Perform setup for data generator
     """
-    def setup(self):
+    def _setup(self):
+        self._setupIdDataset()
+        self._setupFirstNameDataset()
+        self._setupLastNameDataset()
+
+
+    """
+    Setup the ID suffix list
+    """
+    def _setupIdDataset(self):
+        suf = 201000000
+        for i in range(10):
+            self._idSuffix.append(suf)
+            suf += 100000
+
+
+    """
+    Load data set of first names into list
+    """
+    def _setupFirstNameDataset(self):
+        # Todo: load actual data set into list
+        self._fNames.append("jane")
+        self._fNames.append("john")
+        self._fNames.append("bob")
+        self._fNames.append("lucy")
+        self._fNames.append("sally") # shoutout to J.H.
+
+
+    """
+    Load data set of last names into list
+    """
+    def _setupLastNameDataset(self):
+        # Todo: load actual data set into list
+        self._lNames.append("dole")
+        self._lNames.append("doe")
+        self._lNames.append("hall")
+        self._lNames.append("rupp")
+        self._lNames.append("kipchoge")
