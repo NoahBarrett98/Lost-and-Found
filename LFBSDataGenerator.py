e54659f1502feeeb9d76523ea41fbe6dce4beded
Travis MacDonald (x2015khn) <x2015khn@stfx.ca>
2020-02-26 20:04:39 -0400

    Init python files

---
+++ LFBSDataGenerator.py
@@ -0,0 +1,10 @@
+class LFBSDataGenerator:
+
+    """
+    Constructor
+    """
+    def __init__(self):
+        return None
+
+    def __repr__(self):
+        return "I am a Lost and Found / Buy and Sell random data generator"
---
+++ LFBSTest.py
@@ -0,0 +1,4 @@
+from LFBSDataGenerator import LFBSDataGenerator as gen
+
+generator = gen()
+print(generator)
